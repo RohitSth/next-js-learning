450c822cd340f442333ba77f57f3096ad741015e
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-15 09:37:59 +0545

    implemented suspense for streaming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+const { nextui } = require('@nextui-org/react');
 
 const config: Config = {
   content: [
@@ -27,6 +28,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  darkMode: 'class',
+  plugins: [nextui(), require('@tailwindcss/forms')],
 };
 export default config;
