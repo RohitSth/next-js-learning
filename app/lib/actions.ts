4305aeb4f7c119565d666d77d68313391afe3c20
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-21 09:34:47 +0545

    added upto middleware

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -111,8 +111,6 @@ export async function updateInvoice(
 
 //Delete
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
